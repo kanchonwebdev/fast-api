1b63dfe3d4905cc66dffc259f0de4580b20b615a
kanchonwebdev <88568490+kanchonwebdev@users.noreply.github.com>
2024-06-02 23:47:06 +0600

    update

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+    return {"product_id": item_id, "product_name": q}
